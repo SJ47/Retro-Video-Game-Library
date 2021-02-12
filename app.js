50de64c910f652ed50f90aca60acbd0b2e70016c
Scott Johnston <scott@umachan.co.uk>
2021-02-12 12:19:34 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript loaded');
+
+    // Create the HTML page layout 
+    createPageLayout();
+
+    // Assign the eventlisteners for page elements
+    // startButton.addEventListener("click", handleStartButton);
+})
+
+
+// Create the page layout of HTML elements
+createPageLayout = function () {
+    // Create page header section
+    createPageLayoutHeader()
+
+    // Create page main section
+    createPageLayoutMain()
+}
+
+// Create page main section
+createPageLayoutMain = function () {
+
+    // Create main section after the header section
+    main = document.createElement("main");
+    document.body.append(main);
+
+    // Create section section as a child of main section
+    section = document.createElement("section");
+    main.appendChild(section);
+
+    // Create form section as a child of section section
+    form = document.createElement("form");
+    section.appendChild(form);
+
+}
+
+
+// Create page header section
+createPageLayoutHeader = function () {
+    // Create header section at top of body
+    header = document.createElement("header");
+    document.body.append(header);
+
+    // Create h1 page title
+    h1 = document.createElement("h1")
+    h1.append("My Retro Video Game Library")
+    header.appendChild(h1)
+
+    // Create nav
+    nav = document.createElement("nav");
+    header.appendChild(nav);
+
+    // Create nav ul
+    navUl = document.createElement("ul");
+    nav.appendChild(navUl);
+
+    // Create nav ul li items
+    navUlLi = document.createElement("li")
+    navUlLi.append("Create form in the DOM")
+    navUl.appendChild(navUlLi)
+
+    navUlLi = document.createElement("li")
+    navUlLi.append("Delete Library List")
+    navUl.appendChild(navUlLi)
+
+    navUlLi = document.createElement("li")
+    navUlLi.append("Reset Whole Page")
+    navUl.appendChild(navUlLi)
+}
